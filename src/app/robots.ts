ab880717edbff666712286e613c9feb2bb12afd0
Ashok <56536970+cygnuxxs@users.noreply.github.com>
2025-08-06 10:09:54 +0530

    Changes robots.ts typo

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,5 @@
 import type { MetadataRoute } from "next";
-export default function manifest(): MetadataRoute.Robots {
+export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
         {
@@ -8,6 +8,5 @@ export default function manifest(): MetadataRoute.Robots {
         },
         ],
         sitemap: "https://audiovibes.vercel.app/sitemap.xml", // Replace with your actual sitemap URL
-        host: "https://audiovibes.vercel.app", // Replace with your actual domain
     };
     }
